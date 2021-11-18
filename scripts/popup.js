e3d65afd7ccecf264102aea5d936bb013f58d423
Jeongwoo Hyun <jh986@cornell.edu>
2021-11-17 21:53:03 -0500

    basic implementation of js for teams

---
+++ scripts/popup.js
@@ -0,0 +1,13 @@
+// Reference: https://www.w3schools.com/howto/howto_css_modals.asp
+// Because there is no code snippet on how to implement popup banners from the resource handout, I referred to an online tutorial.
+// The code written is my own work.
+var modal = document.getElementById("founder");
+var span = document.getElementsByClassName("close")[0];
+
+$("#MiYan").click(function() {
+  modal.style.display = "block";
+})
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
