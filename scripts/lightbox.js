07796f734f54fd7757c152f49d029bb32291d126
mmc324 <mmc324@cornell.edu>
2021-11-30 18:30:15 -0500

    fix vertical centering on Our Tech gallery

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -2,6 +2,7 @@ $("#wok_a").click(function(){
     if ($(window).width()>800){
         $(".exit").removeClass("hidden");
         $("#wok-a-popup").removeClass("hidden");
+        $("#wok-a-div").removeClass("hidden");
         $(".dim").show();
         $("body").css("overflow", "hidden");
     }
@@ -11,6 +12,7 @@ $("#wok_b").click(function(){
     if ($(window).width()>800){
         $(".exit").removeClass("hidden");
         $("#wok-b-popup").removeClass("hidden");
+        $("#wok-b-div").removeClass("hidden");
         $(".dim").show();
         $("body").css("overflow", "hidden");
     }
@@ -20,6 +22,7 @@ $("#wok_c").click(function(){
     if ($(window).width()>800){
         $(".exit").removeClass("hidden");
         $("#wok-c-popup").removeClass("hidden");
+        $("#wok-c-div").removeClass("hidden");
         $(".dim").show();
         $("body").css("overflow", "hidden");
     }
@@ -29,16 +32,16 @@ $("#cooking-diagram").click(function(){
     if ($(window).width()>800){
         $(".exit").removeClass("hidden");
         $("#cooking-popup").removeClass("hidden");
+        $("#cooking-div").removeClass("hidden");
         $(".dim").show();
         $("body").css("overflow", "hidden");
     }
 });
 
 $(".exit").click(function(){
-    if ($(window).width()>800){
-        $(".large").addClass("hidden");
-        $(".dim").hide();
-        $(".exit").addClass("hidden");
-        $("body").css("overflow", "scroll");
-    }
+    $(".large").addClass("hidden");
+    $(".vertical").addClass("hidden");
+    $(".dim").hide();
+    $(".exit").addClass("hidden");
+    $("body").css("overflow", "scroll");
 });
