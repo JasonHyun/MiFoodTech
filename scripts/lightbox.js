f8d2fb6a123c65657a5f54de1b8e40a11797543a
mmc324 <mmc324@cornell.edu>
2021-11-29 20:16:40 -0500

    fix mobile wok spacing, try to fix interactivity

--- scripts/lightbox.js
+++ scripts/lightbox.js
@@ -1,41 +1,44 @@
 // when window > 800px:
 // $(window).resize(function(){
-//     if ($(window).width()> 800){
-        $("#wok_a").click(function(){
-            $(".exit").removeClass("hidden");
-            $("#wok-a-popup").removeClass("hidden");
-            $(".dim").show();
-            $("body").css("overflow", "hidden");
-        });
-
-        $("#wok_b").click(function(){
-            $(".exit").removeClass("hidden");
-            $("#wok-b-popup").removeClass("hidden");
-            $(".dim").show();
-            $("body").css("overflow", "hidden");
-        });
-
-        $("#wok_c").click(function(){
-            $(".exit").removeClass("hidden");
-            $("#wok-c-popup").removeClass("hidden");
-            $(".dim").show();
-            $("body").css("overflow", "hidden");
-        });
-
-        $("#cooking-diagram").click(function(){
-            $(".exit").removeClass("hidden");
-            $("#cooking-popup").removeClass("hidden");
-            $(".dim").show();
-            $("body").css("overflow", "hidden");
-        });
-
-        $(".exit").click(function(){
-            $(".large").addClass("hidden");
-            $(".dim").hide();
-            $(".exit").addClass("hidden");
-            $("body").css("overflow", "scroll");
-        });
-//     } else{
-//         return;
+//     if ($(window).width()< 600){
+//         $(".large").css("display", "none");
+//         $(".dim").css("display", "none");
+//         $(".exit").css("display", "none");
+//         $("body").css("overflow", "scroll");
 //     }
 // });
+
+$("#wok_a").click(function(){
+    $(".exit").removeClass("hidden");
+    $("#wok-a-popup").removeClass("hidden");
+    $(".dim").show();
+    $("body").css("overflow", "hidden");
+});
+
+$("#wok_b").click(function(){
+    $(".exit").removeClass("hidden");
+    $("#wok-b-popup").removeClass("hidden");
+    $(".dim").show();
+    $("body").css("overflow", "hidden");
+});
+
+$("#wok_c").click(function(){
+    $(".exit").removeClass("hidden");
+    $("#wok-c-popup").removeClass("hidden");
+    $(".dim").show();
+    $("body").css("overflow", "hidden");
+});
+
+$("#cooking-diagram").click(function(){
+    $(".exit").removeClass("hidden");
+    $("#cooking-popup").removeClass("hidden");
+    $(".dim").show();
+    $("body").css("overflow", "hidden");
+});
+
+$(".exit").click(function(){
+    $(".large").addClass("hidden");
+    $(".dim").hide();
+    $(".exit").addClass("hidden");
+    $("body").css("overflow", "scroll");
+});
