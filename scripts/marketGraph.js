19b13b4de49970446572826c6949c015cd0224e5
Claire Jiang <cj337@cornell.edu>
2021-12-01 12:39:34 -0500

    implemented re-design changes in industry page

--- scripts/marketGraph.js
+++ scripts/marketGraph.js
@@ -1,29 +1,29 @@
 $("#TAM").click(function() {
-  $("#TAMtext").removeClass("hidden");
-  $("#TAMclose").removeClass("hidden");
+  $("#TAMtext").removeClass("hidden-text");
+  $("#TAMclose").removeClass("hidden-text");
 });
 
 $("#TAMclose").click(function() {
-  $("#TAMtext").addClass("hidden");
-  $("#TAMclose").addClass("hidden");
+  $("#TAMtext").addClass("hidden-text");
+  $("#TAMclose").addClass("hidden-text");
 });
 
 $("#SAM").click(function() {
-  $("#SAMtext").removeClass("hidden");
-  $("#SAMclose").removeClass("hidden");
+  $("#SAMtext").removeClass("hidden-text");
+  $("#SAMclose").removeClass("hidden-text");
 });
 
 $("#SAMclose").click(function() {
-  $("#SAMtext").addClass("hidden");
-  $("#SAMclose").addClass("hidden");
+  $("#SAMtext").addClass("hidden-text");
+  $("#SAMclose").addClass("hidden-text");
 });
 
 $("#SOM").click(function() {
-  $("#SOMtext").removeClass("hidden");
-  $("#SOMclose").removeClass("hidden");
+  $("#SOMtext").removeClass("hidden-text");
+  $("#SOMclose").removeClass("hidden-text");
 });
 
 $("#SOMclose").click(function() {
-  $("#SOMtext").addClass("hidden");
-  $("#SOMclose").addClass("hidden");
+  $("#SOMtext").addClass("hidden-text");
+  $("#SOMclose").addClass("hidden-text");
 });
